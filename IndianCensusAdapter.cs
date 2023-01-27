1d6e7bad8e3e2eeb73ea468e62c35854db8410fb
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-27 23:58:26 +0530

    Create IndianCensusAdapter.cs

---
+++ IndianCensusAdapter.cs
@@ -0,0 +1,41 @@
+using CensusAnalyserProgram.DTO;
+using CensusAnalyserProgram.POCO;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace CensusAnalyserProgram
+{
+    class IndianCensusAdapter: CensusAdapter
+    {
+        string[] censusData;
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string,CensusDTO> LoadCensusData(string csvFilePath,string dataHeaders)
+        {
+            dataMap = new Dictionary<string, CensusDTO>();
+            censusData = GetCensusData(csvFilePath, dataHeaders);
+            foreach (string data in censusData.Skip(1))
+            {
+                if (!data.Contains(","))
+                {
+                    throw new CensusAnalyserException("File contains wrong delimeter", CensusAnalyserException.ExceptionType.INCORRECT_DELIMETER);
+                }
+
+                string[] column = data.Split(",");
+
+                if (csvFilePath.Contains("IndiaStateCode.csv"))
+                {
+                    dataMap.Add(column[1], new CensusDTO(new StateCodeDAO(column[0], column[1], column[2], column[3])));
+                }
+
+                if (csvFilePath.Contains("IndiaStateCensusData.csv"))
+                {
+                    dataMap.Add(column[0], new CensusDTO(new CensusDataDAO(column[0], column[1], column[2], column[3])));
+                }
+            }
+            return dataMap.ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+}
