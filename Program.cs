935ba4eb5e4e6105ba13d8b1d676b8ced85a594b
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-27 23:51:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome To Cab Invoice Generator Program. *****");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
