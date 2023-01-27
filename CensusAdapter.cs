66fd45373d50f855c3a1e1024375a70fdd1d24ce
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-27 23:55:54 +0530

    Create CensusAdapter.cs

---
+++ CensusAdapter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyserProgram
+{
+    public abstract class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilePath, string dataHeaders)
+        {
+            string[] censusData;
+            if (!File.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File not found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            if (Path.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Invalid file type", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != dataHeaders)
+            {
+                throw new CensusAnalyserException("Incorrect header in Data", CensusAnalyserException.ExceptionType.INCORRECR_HEADER);
+            }
+            return censusData;
+        }
+    }
+}
