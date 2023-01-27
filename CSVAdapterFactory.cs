b9c557071f2c82baf36536cf8dea37ee5128da8f
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-27 23:55:11 +0530

    Create CSVAdapterFactory.cs

---
+++ CSVAdapterFactory.cs
@@ -0,0 +1,23 @@
+using CensusAnalyserProgram.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserProgram
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string,CensusDTO> LoadCsvData(CensusAnalyzer.Country country,string csvFilePath,string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyzer.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyzer.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath,dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No such country",CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
