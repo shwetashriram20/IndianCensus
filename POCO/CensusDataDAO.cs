dd596f4cba834683eaf058b655fb272b60935092
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-28 00:14:35 +0530

    Create CensusDataDAO.cs

---
+++ POCO/CensusDataDAO.cs
@@ -0,0 +1,24 @@
+using CensusAnalyserProgram.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserProgram.POCO
+{
+    public class CensusDataDAO
+    {
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+
+        public CensusDataDAO(string state,string population,string area,string density)
+        {
+            this.state = state;
+            this.population = Convert.ToInt32(population);
+            this.area = Convert.ToInt32(area);
+            this.density = Convert.ToInt32(density);
+        }
+
+    }
+}
